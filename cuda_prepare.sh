ec035bd5ed7123f05bba03083e84b55d92a5bea8
dimitar-popov-fintegral <dimitar.popov@fintegral.com>
2019-10-21 22:48:26 +0200

    minor fixes to scrip code

--- cuda_prepare.sh
+++ cuda_prepare.sh
@@ -4,11 +4,11 @@ set -o errrexit
 set -o xtrace
 
 # get the standard C-compiler
-sudo apt install gcc
-gcc --version
+sudo apt-get --assume-yes gcc
+echo gcc --version
 
 # get the standard NVIDIA/CUDA installer
 wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
-sudo sh cuda_10.1.243_418.87.00_linux.run --silent --drive --toolkit
+sudo sh cuda_10.1.243_418.87.00_linux.run --silent --driver --toolkit
 
 
