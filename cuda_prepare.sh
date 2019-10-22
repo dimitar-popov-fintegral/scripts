744fb03b5a70012bdaa9fcfc6bc75d390025163c
dimitar-popov-fintegral <dimich2007@gmail.com>
2019-10-22 20:22:16 +0000

    added new cuda preparation script, awaiting test on cuda enabled machine

--- cuda_prepare.sh
+++ cuda_prepare.sh
@@ -4,11 +4,24 @@ set -o errrexit
 set -o xtrace
 
 # get the standard C-compiler
-sudo apt-get --assume-yes gcc
-echo gcc --version
+sudo apt-get install -y gcc
+echo `gcc --version`
 
 # get the standard NVIDIA/CUDA installer
-wget http://developer.download.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.243_418.87.00_linux.run
-sudo sh cuda_10.1.243_418.87.00_linux.run --silent --driver --toolkit
+curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+rm cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+
+sudo apt-get install -y cuda-10-0
+
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64' >> ~/.bashrc
+
+# TO-TEST MANUALLY
+# source ~/.bashrc
+# nvcc --version
+# nvidia-smi
 
 
